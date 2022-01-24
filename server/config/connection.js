5e4f6dfacdadc43ae53b5eed3a09c4e236ffae23
Konner Hartman <konner.hartman@yahoo.com>
2022-01-24 15:28:02 -0600

    changed url

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/redux-reuse-recycle', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
