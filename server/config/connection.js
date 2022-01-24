a86b7a0fa0436ec3c6bc400687b8c187611615f7
Konner Hartman <konner.hartman@yahoo.com>
2022-01-24 15:40:23 -0600

    changed db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect('process.env.mongodb+srv://khartman22-admin:k_Hart1022@cluster0.gllsx.mongodb.net/mernshopping?retryWrites=true&w=majority' || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
